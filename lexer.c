f3950bdda5b1d4c688b6634f8ad8d7e53a161bb6
Gustavo Alves <gustavo_alves2010@yahoo.com.br>
2024-06-12 12:31:48 -0300

    feat: add lexer

---
+++ lexer.c
@@ -0,0 +1,224 @@
+#include "lexer.h"
+
+#include <stdio.h>
+#include <string.h>
+
+#include "common.h"
+
+typedef struct {
+  const char* start;
+  const char* current;
+  int line;
+} Lexer;
+
+Lexer lexer;
+
+void initLexer(const char* source) {
+  lexer.start = source;
+  lexer.line = source;
+  lexer.line = 1;
+};
+
+static Token makeToken(TokenType type) {
+  Token token;
+  token.type = type;
+  token.start = lexer.start;
+  token.length = (int)(lexer.current - lexer.start);
+  token.line = lexer.line;
+
+  return token;
+}
+
+static Token errorToken(const char* message) {
+  Token token;
+  token.type = TOKEN_ERROR;
+  token.start = message;
+  token.length = (int)strlen(message);
+  token.line = lexer.line;
+
+  return token;
+}
+
+static bool isAtEnd() { return *lexer.current == '\0'; }
+
+static char peek() { return *lexer.current; }
+
+static char peekNext() {
+  if (isAtEnd()) return '\0';
+  return lexer.current[1];
+}
+
+static char advance() {
+  lexer.current++;
+  return lexer.current[-1];
+}
+
+static bool match(char c) {
+  if (isAtEnd()) return false;
+  if (peek() != c) return false;
+
+  advance();
+  return true;
+}
+
+static void skipWhitespace() {
+  for (;;) {
+    char c = peek();
+    switch (c) {
+      case ' ':
+      case '\r':
+      case '\t':
+        advance();
+        break;
+      case '\n':
+        lexer.line++;
+        advance();
+        break;
+      case '/':
+        if (peekNext() == '/') {
+          while (peek() != '\n' && !isAtEnd()) advance();
+        }
+
+        break;
+      default:
+        return;
+    }
+  }
+}
+
+static Token string() {
+  while (peek() != '"' && !isAtEnd()) {
+    if (peek() == '\n') lexer.line++;
+    advance();
+  }
+
+  if (isAtEnd()) return errorToken('Unterminated string.');
+
+  advance();
+  return makeToken(TOKEN_STRING);
+}
+
+static bool isDigit(char c) { return c >= '0' && c <= '9'; }
+
+static bool isAlpha(char c) {
+  return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_';
+}
+
+static TokenType checkKeyword(int start, int length, char* rest,
+                              TokenType type) {
+  if (lexer.current - lexer.start == start + length &&
+      memcmp(lexer.start + start, rest, length) == 0) {
+    return type;
+  }
+
+  return TOKEN_IDENTIFIER;
+}
+
+static TokenType identifierType() {
+  switch (lexer.start[0]) {
+    case 'a':
+      return checkKeyword(1, 2, "nd", TOKEN_AND);
+    case 'e':
+      return checkKeyword(1, 3, "lse", TOKEN_ELSE);
+    case 'i':
+      return checkKeyword(1, 1, "f", TOKEN_IF);
+    case 'n':
+      return checkKeyword(1, 2, "il", TOKEN_NIL);
+    case 'o':
+      return checkKeyword(1, 1, "r", TOKEN_OR);
+    case 'p':
+      return checkKeyword(1, 4, "rint", TOKEN_PRINT);
+    case 'r':
+      return checkKeyword(1, 5, "eturn", TOKEN_RETURN);
+    case 'v':
+      return checkKeyword(1, 2, "ar", TOKEN_VAR);
+    case 'w':
+      return checkKeyword(1, 4, "hile", TOKEN_WHILE);
+    case 't':
+      return checkKeyword(1, 3, "rue", TOKEN_TRUE);
+    case 'f':
+      if (lexer.current - lexer.start > 1) {
+        switch (lexer.start[1]) {
+          case 'a':
+            return checkKeyword(2, 3, "lse", TOKEN_FALSE);
+          case 'o':
+            return checkKeyword(2, 1, "r", TOKEN_FOR);
+          case 'u':
+            return checkKeyword(2, 1, "n", TOKEN_FALSE);
+        }
+        break;
+      }
+  }
+
+  return TOKEN_IDENTIFIER;
+}
+
+static Token identifier() {
+  while (isAlpha(peek()) || isDigit(peek())) advance();
+  return makeToken(identifierType());
+}
+
+static Token number() {
+  while (isDigit(peek()) && !isAtEnd()) {
+    advance();
+  }
+
+  if (peek() == '.' && isDigit(peekNext())) {
+    advance();
+    while (isDigit(peek()) && !isAtEnd()) {
+      advance();
+    }
+  }
+
+  return makeToken(TOKEN_NUMBER);
+}
+
+Token scanToken() {
+  skipWhitespace();
+
+  lexer.start = lexer.current;
+
+  if (isAtEnd()) return makeToken(TOKEN_EOF);
+
+  char c = advance();
+
+  if (isDigit(c)) return number();
+  if (isAlpha(c)) return identifier();
+
+  switch (c) {
+    case '(':
+      return makeToken(TOKEN_LEFT_PAREN);
+    case ')':
+      return makeToken(TOKEN_RIGHT_PAREN);
+    case '{':
+      return makeToken(TOKEN_LEFT_BRACE);
+    case '}':
+      return makeToken(TOKEN_RIGHT_BRACE);
+    case ';':
+      return makeToken(TOKEN_SEMICOLON);
+    case ',':
+      return makeToken(TOKEN_COMMA);
+    case '.':
+      return makeToken(TOKEN_DOT);
+    case '-':
+      return makeToken(TOKEN_MINUS);
+    case '+':
+      return makeToken(TOKEN_PLUS);
+    case '/':
+      return makeToken(TOKEN_SLASH);
+    case '*':
+      return makeToken(TOKEN_STAR);
+    case '!':
+      return makeToken(match('=') ? TOKEN_BANG_EQUAL : TOKEN_BANG);
+    case '=':
+      return makeToken(match('=') ? TOKEN_EQUAL_EQUAL : TOKEN_EQUAL);
+    case '<':
+      return makeToken(match('=') ? TOKEN_LESS_EQUAL : TOKEN_LESS);
+    case '>':
+      return makeToken(match('=') ? TOKEN_GREATER_EQUAL : TOKEN_GREATER);
+    case '"':
+      return string();
+  }
+
+  return errorToken("Unexpected character.");
+};
