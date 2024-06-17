104d81bc608e9dc08b56aa079e53c5846e875812
Gustavo Alves <gustavo_alves2010@yahoo.com.br>
2024-06-17 18:29:06 -0300

    fix: lexer FUN token

--- lexer.c
+++ lexer.c
@@ -145,7 +145,7 @@ static TokenType identifierType() {
           case 'o':
             return checkKeyword(2, 1, "r", TOKEN_FOR);
           case 'u':
-            return checkKeyword(2, 1, "n", TOKEN_FALSE);
+            return checkKeyword(2, 1, "n", TOKEN_FUN);
         }
         break;
       }
