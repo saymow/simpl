e41192b77ea0aa898dc13af9de38126e3664ce02
Gustavo Alves <gustavo_alves2010@yahoo.com.br>
2024-06-21 22:02:06 -0300

    feat: tokenize from keyword

--- lexer.c
+++ lexer.c
@@ -5,26 +5,33 @@
 
 #include "common.h"
 
-typedef struct {
-  const char* start;
-  const char* current;
-  int line;
-} Lexer;
-
-Lexer lexer;
+Lexer globalLexer;
+Lexer* lexer;
 
 void initLexer(const char* source) {
-  lexer.start = source;
-  lexer.current = source;
-  lexer.line = 1;
+  lexer = &globalLexer;
+  lexer->start = source;
+  lexer->current = source;
+  lexer->line = 1;
 };
+
+void stackLexer(Lexer* nextLexer, const char* source) {
+  nextLexer->start = source;
+  nextLexer->current = source;
+  nextLexer->line = 1;
+  nextLexer->enclosing = lexer;
+
+  lexer = nextLexer;
+}
+
+void popLexer() { lexer = lexer->enclosing; }
 
 static Token makeToken(TokenType type) {
   Token token;
   token.type = type;
-  token.start = lexer.start;
-  token.length = (int)(lexer.current - lexer.start);
-  token.line = lexer.line;
+  token.start = lexer->start;
+  token.length = (int)(lexer->current - lexer->start);
+  token.line = lexer->line;
 
   return token;
 }
@@ -34,23 +41,23 @@ static Token errorToken(const char* message) {
   token.type = TOKEN_ERROR;
   token.start = message;
   token.length = (int)strlen(message);
-  token.line = lexer.line;
+  token.line = lexer->line;
 
   return token;
 }
 
-static bool isAtEnd() { return *lexer.current == '\0'; }
+static bool isAtEnd() { return *lexer->current == '\0'; }
 
-static char peek() { return *lexer.current; }
+static char peek() { return *lexer->current; }
 
 static char peekNext() {
   if (isAtEnd()) return '\0';
-  return lexer.current[1];
+  return lexer->current[1];
 }
 
 static char advance() {
-  lexer.current++;
-  return lexer.current[-1];
+  lexer->current++;
+  return lexer->current[-1];
 }
 
 static bool match(char c) {
@@ -71,7 +78,7 @@ static void skipWhitespace() {
         advance();
         break;
       case '\n':
-        lexer.line++;
+        lexer->line++;
         advance();
         break;
       case '/':
@@ -89,7 +96,7 @@ static void skipWhitespace() {
 
 static Token string() {
   while (peek() != '"' && !isAtEnd()) {
-    if (peek() == '\n') lexer.line++;
+    if (peek() == '\n') lexer->line++;
     advance();
   }
 
@@ -107,8 +114,8 @@ static bool isAlpha(char c) {
 
 static TokenType checkKeyword(int start, int length, char* rest,
                               TokenType type) {
-  if (lexer.current - lexer.start == start + length &&
-      memcmp(lexer.start + start, rest, length) == 0) {
+  if (lexer->current - lexer->start == start + length &&
+      memcmp(lexer->start + start, rest, length) == 0) {
     return type;
   }
 
@@ -116,12 +123,12 @@ static TokenType checkKeyword(int start, int length, char* rest,
 }
 
 static TokenType identifierType() {
-  switch (lexer.start[0]) {
+  switch (lexer->start[0]) {
     case 'a':
       return checkKeyword(1, 2, "nd", TOKEN_AND);
     case 'e':
-      if (lexer.current - lexer.start > 1) {
-        switch (lexer.start[1]) {
+      if (lexer->current - lexer->start > 1) {
+        switch (lexer->start[1]) {
           case 'l':
             return checkKeyword(2, 2, "se", TOKEN_ELSE);
           case 'x':
@@ -131,8 +138,8 @@ static TokenType identifierType() {
       }
     default:
     case 'i':
-      if (lexer.current - lexer.start > 1) {
-        switch (lexer.start[1]) {
+      if (lexer->current - lexer->start > 1) {
+        switch (lexer->start[1]) {
           case 'm':
             return checkKeyword(2, 4, "port", TOKEN_IMPORT);
           case 'f':
@@ -155,8 +162,8 @@ static TokenType identifierType() {
     case 's':
       return checkKeyword(1, 4, "uper", TOKEN_SUPER);
     case 't':
-      if (lexer.current - lexer.start > 1) {
-        switch (lexer.start[1]) {
+      if (lexer->current - lexer->start > 1) {
+        switch (lexer->start[1]) {
           case 'r':
             return checkKeyword(2, 2, "ue", TOKEN_TRUE);
           case 'h':
@@ -167,14 +174,16 @@ static TokenType identifierType() {
     case 'c':
       return checkKeyword(1, 4, "lass", TOKEN_CLASS);
     case 'f':
-      if (lexer.current - lexer.start > 1) {
-        switch (lexer.start[1]) {
+      if (lexer->current - lexer->start > 1) {
+        switch (lexer->start[1]) {
           case 'a':
             return checkKeyword(2, 3, "lse", TOKEN_FALSE);
           case 'o':
             return checkKeyword(2, 1, "r", TOKEN_FOR);
           case 'u':
             return checkKeyword(2, 1, "n", TOKEN_FUN);
+          case 'r':
+            return checkKeyword(2, 2, "om", TOKEN_FROM);
         }
         break;
       }
@@ -206,7 +215,7 @@ static Token number() {
 Token scanToken() {
   skipWhitespace();
 
-  lexer.start = lexer.current;
+  lexer->start = lexer->current;
 
   if (isAtEnd()) return makeToken(TOKEN_EOF);
 
@@ -250,5 +259,5 @@ Token scanToken() {
       return string();
   }
 
-  return errorToken("Unexpected character.");
+  return errorToken("Unexpected character->");
 };
