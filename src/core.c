aa4d608023ee57560d40ae8a3704c14aa45b5417
Gustavo Alves <gustavo_alves2010@yahoo.com.br>
2024-07-15 20:45:01 -0300

    feat: add Array.new static method

--- src/core.c
+++ src/core.c
@@ -126,217 +126,235 @@ static inline bool __nativeArrayLength(int argCount, Value* args) {
 }
 
 static inline bool __nativeArrayPush(int argCount, Value* args) {
-    if (!__arityEqualCheck(1, argCount)) return false;
+  if (!__arityEqualCheck(1, argCount)) return false;
 
-    ObjArray* array = AS_ARRAY(*args);
-    Value value = *(++args);
+  ObjArray* array = AS_ARRAY(*args);
+  Value value = *(++args);
 
-    if (array->list.capacity < array->list.count + 1) {
-        int oldCapacity = array->list.capacity;
-        array->list.capacity = GROW_CAPACITY(oldCapacity);
-        array->list.values = GROW_ARRAY(Value, array->list.values, oldCapacity, array->list.capacity);
-    }
+  if (array->list.capacity < array->list.count + 1) {
+    int oldCapacity = array->list.capacity;
+    array->list.capacity = GROW_CAPACITY(oldCapacity);
+    array->list.values = GROW_ARRAY(Value, array->list.values, oldCapacity, array->list.capacity);
+  }
 
-    array->list.values[array->list.count++] = value;
+  array->list.values[array->list.count++] = value;
 
-    push(NUMBER_VAL(array->list.count));
+  push(NUMBER_VAL(array->list.count));
 
-    return true;
+  return true;
 }
 
 static inline bool __nativeArrayPop(int argCount, Value* args) {
-    if (!__arityEqualCheck(0, argCount)) return false;
+  if (!__arityEqualCheck(0, argCount)) return false;
 
-    ObjArray* array = AS_ARRAY(*args);
+  ObjArray* array = AS_ARRAY(*args);
 
-    if (array->list.count == 0) {
-        push(NIL_VAL);
-        
-        return true;
-    }
+  if (array->list.count == 0) {
+    push(NIL_VAL);
+    return true;
+  }
 
-    Value value = array->list.values[--array->list.count];
+  Value value = array->list.values[--array->list.count];
 
-    // todo: idk if this should be handled by the garbage collector
-    if (array->list.capacity > ARRAY_INITIAL_CAPACITY && array->list.count / (double) array->list.capacity  < ARRAY_MIN_CAPACITY_RATIO) {
-        int oldCapacity = array->list.capacity;
-        array->list.capacity = SHRINK_CAPACITY(array->list.capacity);
-        array->list.values = GROW_ARRAY(Value, array->list.values, oldCapacity, array->list.capacity);
-    }
+  // todo: idk if this should be handled by the garbage collector
+  if (array->list.capacity > ARRAY_INITIAL_CAPACITY && array->list.count / (double) array->list.capacity  < ARRAY_MIN_CAPACITY_RATIO) {
+    int oldCapacity = array->list.capacity;
+    array->list.capacity = SHRINK_CAPACITY(array->list.capacity);
+    array->list.values = GROW_ARRAY(Value, array->list.values, oldCapacity, array->list.capacity);
+  }
 
-    push(value);
-    
-    return true; 
+  push(value);
+  
+  return true; 
 }
 
 static inline bool __nativeArrayUnshift(int argCount, Value* args) {
-    if (!__arityEqualCheck(1, argCount)) return false;
+  if (!__arityEqualCheck(1, argCount)) return false;
 
-    ObjArray* array = AS_ARRAY(*args);
-    Value value = *(++args);
+  ObjArray* array = AS_ARRAY(*args);
+  Value value = *(++args);
 
-    if (array->list.capacity < array->list.count + 1) {
-        int oldCapacity = array->list.capacity;
-        array->list.capacity = GROW_CAPACITY(oldCapacity);
-        array->list.values = GROW_ARRAY(Value, array->list.values, oldCapacity, array->list.capacity);
-    }
+  if (array->list.capacity < array->list.count + 1) {
+    int oldCapacity = array->list.capacity;
+    array->list.capacity = GROW_CAPACITY(oldCapacity);
+    array->list.values = GROW_ARRAY(Value, array->list.values, oldCapacity, array->list.capacity);
+  }
 
-    for (int idx = array->list.count; idx > 0; idx--) {
-        array->list.values[idx] = array->list.values[idx - 1];
-    }
-    
-    array->list.values[0] = value;
+  for (int idx = array->list.count; idx > 0; idx--) {
+    array->list.values[idx] = array->list.values[idx - 1];
+  }
+  
+  array->list.values[0] = value;
 
-    push(NUMBER_VAL(++array->list.count)); 
+  push(NUMBER_VAL(++array->list.count)); 
 
-    return true;
+  return true;
 }
 
 static inline bool __nativeArrayShift(int argCount, Value* args) {
-    if (!__arityEqualCheck(0, argCount)) return false;
+  if (!__arityEqualCheck(0, argCount)) return false;
 
-    ObjArray* array = AS_ARRAY(*args);
+  ObjArray* array = AS_ARRAY(*args);
 
-    if (array->list.count == 0) {
-        push(NIL_VAL);
-        return true;
-    }
+  if (array->list.count == 0) {
+    push(NIL_VAL);
+    return true;
+  }
 
-    Value value = array->list.values[0];
-    array->list.count--;
+  Value value = array->list.values[0];
+  array->list.count--;
 
-    for (int idx = 0; idx < array->list.count; idx++) {
-        array->list.values[idx] = array->list.values[idx + 1];
-    }
+  for (int idx = 0; idx < array->list.count; idx++) {
+    array->list.values[idx] = array->list.values[idx + 1];
+  }
 
-    // todo: idk if this should be handled by the garbage collector
-    if (array->list.capacity > ARRAY_INITIAL_CAPACITY && array->list.count / (double) array->list.capacity  < ARRAY_MIN_CAPACITY_RATIO) {
-        int oldCapacity = array->list.capacity;
-        array->list.capacity = SHRINK_CAPACITY(array->list.capacity);
-        array->list.values = GROW_ARRAY(Value, array->list.values, oldCapacity, array->list.capacity);
-    }
+  // todo: idk if this should be handled by the garbage collector
+  if (array->list.capacity > ARRAY_INITIAL_CAPACITY && array->list.count / (double) array->list.capacity  < ARRAY_MIN_CAPACITY_RATIO) {
+    int oldCapacity = array->list.capacity;
+    array->list.capacity = SHRINK_CAPACITY(array->list.capacity);
+    array->list.values = GROW_ARRAY(Value, array->list.values, oldCapacity, array->list.capacity);
+  }
 
-    push(value);
-    return true; 
+  push(value);
+  return true; 
 }
 
 static inline bool __nativeArraySlice(int argCount, Value* args) {
-    if (!__arityLessThanOrEqualCheck(2, argCount)) return false;
+  if (!__arityLessThanOrEqualCheck(2, argCount)) return false;
 
-    ObjArray* array = AS_ARRAY(*args);
-    ObjArray* slicedArray = newArray();
-    int start = 0;
-    int end = array->list.count;
+  ObjArray* array = AS_ARRAY(*args);
+  ObjArray* slicedArray = newArray();
+  int start = 0;
+  int end = array->list.count;
 
-    if (argCount >= 1) {
-        start = SAFE_CONSUME_NUMBER(args, "start");
-        // stard is inclusive
-        start = SAFE_INDEX(start, array->list.count);
-        if (argCount == 2) {
-            end = SAFE_CONSUME_NUMBER(args, "end");
-            // end is exclusive, so we need to be able to access the length-ith positon.
-            // Hence, list.count + 1
-            end = SAFE_INDEX_INCLUSIVE(end, array->list.count);
-        } 
-    }
+  if (argCount >= 1) {
+    start = SAFE_CONSUME_NUMBER(args, "start");
+    // stard is inclusive
+    start = SAFE_INDEX(start, array->list.count);
+    if (argCount == 2) {
+      end = SAFE_CONSUME_NUMBER(args, "end");
+      // end is exclusive, so we need to be able to access the length-ith positon.
+      end = SAFE_INDEX_INCLUSIVE(end, array->list.count);
+    } 
+  }
 
-    for (;start < end; start++) {
-        writeValueArray(&slicedArray->list, array->list.values[start]);        
-    }
+  for (;start < end; start++) {
+    writeValueArray(&slicedArray->list, array->list.values[start]);        
+  }
 
-    push(OBJ_VAL(slicedArray));
-    return true;
+  push(OBJ_VAL(slicedArray));
+  return true;
 }
 
 static inline bool __nativeArrayIndexOf(int argCount, Value* args) {
-    if (!__arityLessThanOrEqualCheck(1, argCount)) return false;
+  if (!__arityLessThanOrEqualCheck(1, argCount)) return false;
 
-    ObjArray* array = AS_ARRAY(*args);
-    Value value = *(++args);
+  ObjArray* array = AS_ARRAY(*args);
+  Value value = *(++args);
 
-    for (int idx = 0; idx < array->list.count; idx++) {
-        if (valuesEqual(array->list.values[idx], value)) {
-            push(NUMBER_VAL(idx));
-            return true;
-        }
+  for (int idx = 0; idx < array->list.count; idx++) {
+    if (valuesEqual(array->list.values[idx], value)) {
+      push(NUMBER_VAL(idx));
+      return true;
     }
+  }
 
-    push(NUMBER_VAL(-1));
-    return true;
+  push(NUMBER_VAL(-1));
+  return true;
 }
 
 static inline bool __nativeArrayInsert(int argCount, Value* args) {
-    if (!__arityMoreThanOrEqualCheck(2, argCount)) return false;
+  if (!__arityMoreThanOrEqualCheck(2, argCount)) return false;
 
-    ObjArray* array = AS_ARRAY(*args);
-    int valuesToInsert = argCount - 1;
-    int insertIndex = SAFE_CONSUME_NUMBER(args, "index");
-    int arrInitialLen = array->list.count;
+  ObjArray* array = AS_ARRAY(*args);
+  int valuesToInsert = argCount - 1;
+  int insertIndex = SAFE_CONSUME_NUMBER(args, "index");
+  int arrInitialLen = array->list.count;
 
-    insertIndex = SAFE_INDEX_INCLUSIVE(insertIndex, array->list.count);
+  insertIndex = SAFE_INDEX_INCLUSIVE(insertIndex, array->list.count);
 
-    // Allocate space at the end for values to be insert
-    for (int idx = 0; idx < valuesToInsert; idx++) {
-        writeValueArray(&array->list, NIL_VAL);
-    }
+  // Allocate space at the end for values to be insert
+  for (int idx = 0; idx < valuesToInsert; idx++) {
+    writeValueArray(&array->list, NIL_VAL);
+  }
 
-    // Shift existing values after insert index to the right by valuesToInsert offset
-    for (int idx = 0; idx < arrInitialLen - insertIndex; idx++) {
-        swap(&array->list, arrInitialLen - 1 - idx, arrInitialLen - 1 - idx + valuesToInsert);
-    }
+  // Shift existing values after insert index to the right by valuesToInsert offset
+  for (int idx = 0; idx < arrInitialLen - insertIndex; idx++) {
+    swap(&array->list, arrInitialLen - 1 - idx, arrInitialLen - 1 - idx + valuesToInsert);
+  }
 
-    // Insert items at their positions    
-    for (int idx = 0; idx < valuesToInsert; idx++) {
-        array->list.values[idx + insertIndex] = *(++args);
-    }
+  // Insert items at their positions    
+  for (int idx = 0; idx < valuesToInsert; idx++) {
+    array->list.values[idx + insertIndex] = *(++args);
+  }
 
-    push(OBJ_VAL(array));
-    return true;
+  push(OBJ_VAL(array));
+  return true;
 }
 
 static inline bool __nativeArrayJoin(int argCount, Value* args) {
-    if (!__arityMoreThanOrEqualCheck(1, argCount)) return false;
+  if (!__arityMoreThanOrEqualCheck(1, argCount)) return false;
 
-    ObjArray* array = AS_ARRAY(*args);
-    ObjString* separator = SAFE_CONSUME_STRING(args, "separator");
-    ObjArray* tmpArray = newArray();
-    int length = (array->list.count - 1) * separator->length;
+  ObjArray* array = AS_ARRAY(*args);
+  ObjString* separator = SAFE_CONSUME_STRING(args, "separator");
+  ObjArray* tmpArray = newArray();
+  int length = (array->list.count - 1) * separator->length;
 
-    for (int idx = 0; idx < array->list.count; idx++) {
-        ObjString* str = toString(array->list.values[idx]);
-        length += str->length;
-        writeValueArray(&tmpArray->list, OBJ_VAL(str));
-    }
+  for (int idx = 0; idx < array->list.count; idx++) {
+    ObjString* str = toString(array->list.values[idx]);
+    length += str->length;
+    writeValueArray(&tmpArray->list, OBJ_VAL(str));
+  }
 
-    char *buffer = ALLOCATE(char, length + 1);
-    
-    // There is no separator after the last element, so iteraate until the last element
-    // printing: (element + separator)* + element
-    int idx = 0;
-    int currentLength = 0;
-    for (; idx < tmpArray->list.count - 1; idx++) {
-        ObjString* str = ((ObjString *)  AS_OBJ(tmpArray->list.values[idx]));
-        
-        memcpy(buffer + currentLength, str->chars, str->length);
-        memcpy(buffer + currentLength + str->length, separator->chars, separator->length);
-        currentLength += str->length + separator->length;
-    }
-
+  char *buffer = ALLOCATE(char, length + 1);
+  
+  // There is no separator after the last element, so iteraate until the last element
+  // printing: (element + separator)* + element
+  int idx = 0;
+  int currentLength = 0;
+  for (; idx < tmpArray->list.count - 1; idx++) {
     ObjString* str = ((ObjString *)  AS_OBJ(tmpArray->list.values[idx]));
+      
     memcpy(buffer + currentLength, str->chars, str->length);
+    memcpy(buffer + currentLength + str->length, separator->chars, separator->length);
+    currentLength += str->length + separator->length;
+  }
 
-    buffer[length] = '\0';
+  ObjString* str = ((ObjString *)  AS_OBJ(tmpArray->list.values[idx]));
+  memcpy(buffer + currentLength, str->chars, str->length);
 
-    push(OBJ_VAL(takeString(buffer, length)));
-    return true;
+  buffer[length] = '\0';
+
+  push(OBJ_VAL(takeString(buffer, length)));
+  return true;
 }
 
 static inline bool __nativeStaticArrayIsArray(int argCount, Value* args) {
-    if (!__arityEqualCheck(1, argCount)) return false;
+  if (!__arityEqualCheck(1, argCount)) return false;
 
-    Value value = *(++args);
-    push(IS_ARRAY(value) ? TRUE_VAL : FALSE_VAL);
-    return true;
+  Value value = *(++args);
+  push(IS_ARRAY(value) ? TRUE_VAL : FALSE_VAL);
+  return true;
+}
+
+static inline bool __nativeStaticArrayNew(int argCount, Value* args) {
+  if (!__arityLessThanOrEqualCheck(1, argCount)) return false;
+
+  ObjArray* array = newArray();
+  array->list.count = argCount == 1 ? SAFE_CONSUME_NUMBER(args, "length") : 0;
+
+  while (array->list.capacity < array->list.count) {
+    array->list.capacity = GROW_CAPACITY(array->list.capacity);
+  }
+
+  array->list.values = GROW_ARRAY(Value, array->list.values, 0, array->list.capacity);
+  
+  for (int idx = 0; idx < array->list.count; idx++) {
+    array->list.values[idx] = NIL_VAL;
+  }
+  
+  push(OBJ_VAL(array));
+  return true;
 }
 
 static void defineNativeFunction(VM* vm, Table* methods, const char* name, NativeFn function) {
@@ -407,6 +425,7 @@ void initializeCore(VM* vm) {
   inherit(vm->metaArrayClass, vm->klass);
 
   defineNativeFunction(vm, &vm->metaArrayClass->methods, "isArray", __nativeStaticArrayIsArray);
+  defineNativeFunction(vm, &vm->metaArrayClass->methods, "new", __nativeStaticArrayNew);
 
   vm->arrayClass = defineNewClass("Array");
   inherit(vm->arrayClass, vm->metaArrayClass);
