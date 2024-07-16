e8b46217e1d20fd3fcce9261e4bace54dd09a45d
Gustavo Alves <gustavo_alves2010@yahoo.com.br>
2024-07-16 12:33:22 -0300

    feat: add Array filter method

--- src/core-inc.h
+++ src/core-inc.h
@@ -15,6 +15,23 @@ const char* coreExtension =
 "\n"
 "        return newArray;\n"
 "    }\n"
+"\n"
+"    // Should we allocate the newArray with the base array length?\n"
+"    // there might be a good heuristic for this decision.\n"
+"    // But in order to do that, we would need access to a native method\n"
+"    // that just upfrontly allocate the memory without changing the length.\n"
+"    filter(callback) {\n"
+"        var newArray = Array.new();\n"
+"        var length = this.length();\n"
+"\n"
+"        for (var idx = 0; idx < length; idx += 1) {\n"
+"            if (callback(this[idx])) {\n"
+"                newArray.push(this[idx]);\n"
+"            }\n"
+"        }\n"
+"\n"
+"        return newArray;\n"
+"    }\n"
 "}";
 
 #endif
