838b482790ff28cc8f61c4977db445c9b20174bf
Gustavo Alves <gustavo_alves2010@yahoo.com.br>
2024-09-07 16:45:33 -0300

    doc: comment common.h file

--- src/common.h
+++ src/common.h
@@ -5,25 +5,40 @@
 #include <stddef.h>
 #include <stdint.h>
 
+// Enable NaN boxing
+// more on NaN boxing: https://piotrduperas.com/posts/nan-boxing
 #define NAN_BOXING
 
+// ASCII Characters uppercase to lowercase offset 
 #define ASCII_UPPERCASE_TO_LOWERCASE_OFFSET 32
 
+// Debug program extensively
 #ifdef DEBUG
     #define DEBUG_LOGS
     #define DEBUG_GC
 #endif
 
+// Debug program execution extensively
 #ifdef DEBUG_LOGS
+    // Log compiled bytecode
     #define DEBUG_PRINT_CODE
+    
+    // Log program execution, i.e, program stack and current instruction
     #define DEBUG_TRACE_EXECUTION
 #endif
 
+// Debug garbage collection extensively
 #ifdef DEBUG_GC
+    // Enable stressed gc mode.
+    // All memory allocations trigger garbage collection.
     #define DEBUG_STRESS_GC
+    
+    // Log all memory allocation and deallocation
+    // This is specially handful for triggering segmentation fault because of freed memory access. 
     #define DEBUG_LOG_GC
 #endif
 
+// Total values handled by 1 byte or uint8_t 
 #define UINT8_COUNT (UINT8_MAX + 1)
 
 #endif
